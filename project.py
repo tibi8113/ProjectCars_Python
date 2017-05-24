a71b9978fa38d38612958cfd132e8cb4401e5d1a
H012982\ik012982i9 <ii.tibi@hotmail.com>
2017-05-24 12:36:24 +0200

    Changes

--- project.py
+++ project.py
@@ -3,10 +3,14 @@ import functions as f
 """-----------------------------------------------------------------------------------------------------------------------------------"""
 
 #Crear coches.
-my_car1 = f.Car('Audi', 'A7', 2017, 'Red', 65000)
-my_car2 = f.Car('Ford', 'Mustang', 2017, 'White', 45000)
+my_car1 = f.Car('Audi', 'A7', 2017, 'Red', 66000)
+my_car2 = f.Car('Audi', 'Q7', 2017, 'Grey', 65000)
+my_car3 = f.Car('Ford', 'Mustang', 2017, 'White', 45000)
+my_car4 = f.Car('Ford', 'Focus RS', 2017, 'Blue', 40000)
 my_electric_car1 = f.ElectricCar('Tesla', 'S' , 2017, 'Deep Blue', 85000)
-my_electric_car2 = f.ElectricCar('Passat', 'GTE', 2017, 'Grey', 48000)
+my_electric_car2 = f.ElectricCar('Tesla', 'X', 2017, 'White', 97000)
+my_electric_car3 = f.ElectricCar('Volkswagen', 'Passat GTE', 2017, 'Grey', 48000)
+my_electric_car4 = f.ElectricCar('Volkswagen', 'Golf GTE', 2017, 'Dark Iron', 40000)
 
 """-----------------------------------------------------------------------------------------------------------------------------------"""
 
@@ -32,12 +36,16 @@ for coche in myWareHouse1.cars:
 opc = input().upper()
 if opc == "AUDI":
     print(my_car1)
-elif opc == "FORD":
     print(my_car2)
+elif opc == "FORD":
+    print(my_car3)
+    print(my_car4)
 elif opc == "TESLA":
     print(my_electric_car1)
-elif opc == "PASSAT":
     print(my_electric_car2)
+elif opc == "PASSAT":
+    print(my_electric_car3)
+    print(my_electric_car4)
 """-----------------------------------------------------------------------------------------------------------------------------------"""
 
 '''
